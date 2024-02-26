be686e34c1388f62b8e6bb52fc7c60aa04075a2c
saifulislam735 <mohimshihab735@gmail.com>
2024-02-26 19:05:42 +0600

    question showed up

--- index.js
+++ index.js
@@ -46,9 +46,10 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
+  //console.log(quizData);
 };
 
 // Displaying quiz on quiz page
@@ -59,17 +60,18 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
+    console.log(data);
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
+    <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
     <p class="text-gray-800 text-sm">${quiz.quetion}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
-</div>`;
+    </div>
+    <div class="grid grid-cols-2 gap-4 mt-5">
+      ${displayQuizOptions(quiz.options, i)}
+    </div>
+    </div>`;
   });
 };
 
