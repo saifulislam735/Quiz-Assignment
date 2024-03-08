52aaa1e804392a606b572b4a8cdddf8c40f6b577
saifulislam735 <mohimshihab735@gmail.com>
2024-03-09 02:24:53 +0600

    Taks question and Answer added in blog

--- index.js
+++ index.js
@@ -185,3 +185,8 @@ document.querySelector("#submit").addEventListener("click", () => {
   }, 1500);
   window.scrollTo(0, 0);
 });
+//---------------------------------------------------------------------------
+document.getElementById('blog-btn').addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+
